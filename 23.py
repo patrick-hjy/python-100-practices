d17399bc38c614d0e9c53ebf05c698e1784fbec6
Jiayi Hu <patrick.hu.96@gmail.com>
2017-07-29 00:23:48 +0800

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/28 22:47
+# @Author  : Patrick.hu
+# @Site    : 
+# @File    : 23.py
+# @Software: PyCharm
+
+# 题目：打印出如下图案（菱形）:
+#    *
+#   ***
+#  *****
+# *******
+#  *****
+#   ***
+#    *
+
+def diamond(n):
