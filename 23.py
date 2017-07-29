7747e7abfe744d3d0cda3e626f894c4003ee8537
Jiayi Hu <patrick.hu.96@gmail.com>
2017-07-29 14:53:56 +0800

    Update 23.py

--- 23.py
+++ 23.py
@@ -16,3 +16,11 @@
 #    *
 
 def diamond(n):
+    for i in range(1,n+1):
+        print(' '*(n-i)+'*'*(2*i-1)+' '*(n-i))
+    for i in range(1,n):
+        print(' '*i+'*'*(2*n-2*i-1)+' '*i)
+
+
+if __name__ == '__main__':
+    diamond(4)
